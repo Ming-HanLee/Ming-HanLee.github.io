16ba4cc125a0d0fec61fe869af23fec90255da14
Ming-HanLee <47711448+Ming-HanLee@users.noreply.github.com>
2019-12-17 01:50:36 +0800

    final project second upload

--- final/js/RPS.js
+++ final/js/RPS.js
@@ -56,21 +56,3 @@ var playAgain = () => {
     $('#again-div').empty();
     setTimeout(() => { location.href = '#playing-section'; }, 500);
 }
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() { myFunction() };
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
